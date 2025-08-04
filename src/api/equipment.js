985d971b439ac6ac34982548411dd97ba49b7b99
yinghong-Lin <2687138072@qq.com>
2025-08-04 10:42:19 +0800

    完善生产控制api接口

--- src/api/equipment.js
+++ src/api/equipment.js
@@ -1,17 +1,17 @@
 import request from './index'
 
 export const equipmentAPI = {
-  // 列表分页
+  // 获取设备
   getEquipmentList: (params) =>
     request.get('/productionControl/getEquipmentList', { params }),
 
-  // 搜索分页
+  // 搜索
   searchEquipment: (params) =>
     request.get('/productionControl/searchEquipment', { params }),
 
   // 单条详情
   getEquipmentById: (equipment_id) =>
-    request.get('/productionControl/getEquipmentById', { params: { equipment_id }}),
+    request.get('/productionControl/getEquipmentById', { params: { equipment_id } }),
 
   // 新增
   addEquipment: (data) =>
@@ -19,7 +19,7 @@ export const equipmentAPI = {
 
   // 删除
   deleteEquipment: (equipment_id) =>
-    request.delete('/productionControl/deleteEquipment', { params: { equipment_id }}),
+    request.delete('/productionControl/deleteEquipment', { params: { equipment_id } }),
 
   // 整体更新
   updateEquipment: (data) =>
@@ -31,15 +31,26 @@ export const equipmentAPI = {
       params: { equipment_id, status }
     }),
 
-  // 规格
+  // 更新规格
   updateEquipmentSpec: (equipment_id, data) =>
     request.patch('/productionControl/updateEquipmentSpec', data, {
       params: { equipment_id }
     }),
 
-  // 设置
+  // 更新参数
   updateEquipmentSetting: (equipment_id, data) =>
     request.patch('/productionControl/updateEquipmentSetting', data, {
       params: { equipment_id }
-    })
+    }),
+  // 单个设备添料
+  singleDeviceAddSubstance: (equipment_id) =>
+    request.post('/productionControl/singleDeviceAddSubstance', null, {
+      params: { equipment_id }
+    }),
+
+  // 所有设备添料
+  allDeviceAddSubstance: (equipment_id) =>
+    request.post('/productionControl/allDeviceAddSubstance', null, {
+      params: { equipment_id }
+    }),
 }
